6a3d85611f954f50ced62bb1ad9cdddff71bdad0
ljin25 <85407041+ljin25@users.noreply.github.com>
2021-06-17 12:10:07 -0400

    Part 1
    Added code for part 1

--- problem-1.go
+++ problem-1.go
@@ -1,1 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	
+//part 1: prints sum of all whole numbers 1 to n 
+
+	var n = /*replace w value for n*/
+	var sum = (n * (n + 1)) / 2
+	fmt.Println(sum)
+	
+}
 
