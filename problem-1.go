df57402689c616508432343b28a53d9da77b6645
ljin25 <85407041+ljin25@users.noreply.github.com>
2021-07-06 10:48:13 -0400

    Update problem-1.go

--- problem-1.go
+++ problem-1.go
@@ -6,7 +6,9 @@ func main() {
 	
 //part 1: prints sum of all whole numbers 1 to n 
 
-	var n = /*replace w value for n*/
+	var n int8
+	
+	n = 10/*replace w value for n*/
 	var sum = (n * (n + 1)) / 2
 	fmt.Println(sum)
 	
