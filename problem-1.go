42e5c597f0b301751433a03dfa6a8d17db570109
ljin25 <85407041+ljin25@users.noreply.github.com>
2021-07-07 11:39:02 -0400

    Combined parts 1 & 2

--- problem-1.go
+++ problem-1.go
@@ -3,16 +3,32 @@ package main
 import "fmt"
 
 func main() {
-	
-//part 1: prints sum of all whole numbers 1 to n 
 
-	fmt.Print(Insert value here: )
-	
+//part 1: sum of all numbers 1 to n
+	fmt.Print("Insert value here: ")
+
 	var n uint8
 	fmt.Scanln(&n)
-	
+
 	var sum = (n * (n + 1)) / 2
 	fmt.Println("Answer:", sum)
-	
-}
 
+//part 2: sum of all multiples of 3 & 5 to n
+	threesAndFives := 0
+
+	fmt.Print("Insert value here: ")
+
+	var x int
+	fmt.Scanln(&x)
+
+	for i := 1; i <= x; i++ {
+		if i%3 == 0 {
+			threesAndFives += i
+		}
+		if i%5 == 0 {
+			threesAndFives += i
+		}
+	}
+	
+	fmt.Println("Answer:", threesAndFives)
+}
