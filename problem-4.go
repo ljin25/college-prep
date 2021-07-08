b465906d8320d647d97b3f079fa67dd27118c41e
ljin25 <85407041+ljin25@users.noreply.github.com>
2021-07-08 14:49:33 -0400

    Update problem-4.go

--- problem-4.go
+++ problem-4.go
@@ -1,21 +1,24 @@
+// Write a program that prints the next 20 leap years.
+
 package main
 
 import "fmt"
 
-func main() {
-   
-var i, n int
-
-
-for i = n; i <= (n+80); i++ {
-	if (i%4) != 0 {
-		break;
-	}
-	
-	fmt.Print("Enter the current year: ")
-	n = 2021
-	fmt.Println("Answer:", i)
+func leapYear(y int) bool {
+	return y%4 == 0 || y%4 == 0 && y%100 != 0
 }
 
-  
+func main() {
+   
+	var currentYear int
+	
+	fmt.Print("Enter the current year: ")
+	fnt.Scanln(&currentYear)
+
+	for i := n; i <= (n+80); i++ {
+		if leapYear(i) {
+			fmt.Println(i)
+		}
+		
+	}
 }
