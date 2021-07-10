b5a02086a541ec9df1fb2b0760a1f85bedefbfab
ljin25 <85407041+ljin25@users.noreply.github.com>
2021-07-10 10:24:48 -0400

    Update problem-1.go

--- problem-1.go
+++ problem-1.go
@@ -1,3 +1,6 @@
+/*Write a program that asks the user for a number n and prints the sum of the numbers 1 to n. 
+Modify the previous program such that only multiples of three or five are considered in the sum, e.g. 3, 5, 6, 9, 10, 12, 15 for n=17.*/
+
 package main
 
 import "fmt"
@@ -13,7 +16,7 @@ func main() {
 	var sum = (n * (n + 1)) / 2
 	fmt.Println("Answer:", sum)
 
-//part 2: sum of all multiples of 3 & 5 to n
+	//part 2: sum of all multiples of 3 & 5 to n (x in this case)
 	threesAndFives := 0
 
 	fmt.Print("Insert value here: ")
