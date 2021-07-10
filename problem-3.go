b1efb9dbeff4a80e537ae8bd66a357b341cfed54
ljin25 <85407041+ljin25@users.noreply.github.com>
2021-07-10 10:27:28 -0400

    Update problem-3.go

--- problem-3.go
+++ problem-3.go
@@ -1,5 +1,7 @@
-/* Write a program that asks the user for a number n and gives them the possibility to choose between 
-computing the sum and computing the product of 1,…,n. */
+/*
+Write a program that asks the user for a number n and gives them the possibility to choose between 
+computing the sum and computing the product of 1,…,n.
+*/
 
 package main
 
