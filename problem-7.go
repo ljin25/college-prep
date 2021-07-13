683c583937d952e7d8deb3fca4c62fe6315a6536
ljin25 <85407041+ljin25@users.noreply.github.com>
2021-07-13 15:18:11 -0400

    Create problem-7.go

---
+++ problem-7.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+  
+	var quantity int
+	fmt.Print("How many numbers? ")
+	fmt.Scanln(&quantity)
+  
+	n := make([]float32, quantity)
+	for i := 0; i < quantity; i++ {
+		fmt.Print("Enter a value: ")
+		fmt.Scanln(&n[i])
+	}
+	for j := 1; j < quantity; j++ {
+		if n[0] < n[j] {
+			n[0] = n[j]
+		}
+	}
+	fmt.Println("The largest value in your list is", n[0])
+}
